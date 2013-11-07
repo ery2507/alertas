08ad69d42d89e8da76d64ee989c57ff2c09fbf0c
ERIKA CAMACHO MOTOLINIA <ery2507.ec@gmail.com>
2013-11-07 16:50:10 -0600

    sonidos
    alertas

---
+++ js/acciones.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+$(document).ready(function(e){// e de evento
+	document.addEventListener("deviceready",function(){
+		$('.btn').tap(function(){
+			var boton =(($(this).attr('class')).split(' '))[1];
+			switch (boton)
+				{
+					case 'b1':
+						navigator.notification.beep(2);
+					break;
+					case 'v1':
+						navigator.notification.vibrate(1000);
+					break;
+				}//switch
+		});//tap
+	},false);// deviceready
+});// ready
+
